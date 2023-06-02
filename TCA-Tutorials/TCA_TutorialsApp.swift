815462cb22173a3399311171574e86e7f63b5353
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-06-02 19:27:58 +0900

    first commit

---
+++ TCA-Tutorials/TCA_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TutorialsApp.swift
+//  TCA-Tutorials
+//
+//  Created by 上條栞汰 on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
